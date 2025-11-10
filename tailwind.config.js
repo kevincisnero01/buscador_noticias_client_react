86d5856cdcd2ed8eb94637141e0544ad5a48007a
Kevin <kevingabriel18@gamil.com>
2025-11-10 07:34:31 -0400

    chore: clean unnecessary files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Rutas para un proyecto React/Vite/Web
+    "./*.html", // Si tienes un index.html en la raíz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
