f63f4d4316d4c1ef6a6acded5af5b7c56dec76ca
Kevin Cisnero <kevingabriel18@gmail.com>
2025-11-16 22:32:06 -0400

    feat(new): Query API and reading the results

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import NewForm from './components/New/NewForm'
 
@@ -6,6 +6,23 @@ function App() {
 
   // State para almacenar la categoría de noticias seleccionada por el usuario.
   const [category, setCategory] = useState('');
+  //State para noticias
+  const [news, setNews] = useState([]);
+
+
+  useEffect(() => {
+
+    const consultAPI = async() =>{
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=945ca81751ca4a12bdc1cf58e480043e`;
+    
+      const result = await fetch(url);
+
+      const data = await result.json();
+
+      setNews(data.articles);
+    }
+    consultAPI();
+  },[category]);
 
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 function Header({title}) {
   return (
     <>
-      <h1 class="text-3xl font-extrabold text-white bg-indigo-700 p-4 text-center">
+      <h1 className="text-3xl font-extrabold text-white bg-indigo-700 p-4 text-center">
           {title}
         </h1>
     </>
